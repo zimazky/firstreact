0fce2c946c09ec8383d55e120971e76b8cec9acc
Andrey Zimatskiy <zimazky@gmail.com>
2021-08-29 00:35:20 +0300

    Передача из модального окна

--- src/components/ParameterButton.jsx
+++ src/components/ParameterButton.jsx
@@ -4,7 +4,6 @@ import styles from './ParameterButton.module.css'
 export default function({parameterName, value = 0., disabled = false, update}) {
   const [isModalOpen,openModal] = React.useState(false)
   const [state, setState] = React.useState(0.)
-  //const refModal = React.useRef()
   console.log('ModalState:', isModalOpen)
   console.log('ParameterState:', state)
 
@@ -18,7 +17,7 @@ export default function({parameterName, value = 0., disabled = false, update}) {
     <div className='button' onClick={()=>{openModal(!isModalOpen)}}>
       <div className={styles.name}>{parameterName}</div>
       <div className={styles.value}>{value}</div>
-      <Modal isOpen={isModalOpen} title={'Set target temperature for Zone'} onSubmit={()=>{openModal(false)}} onCancel={()=>{openModal(false)}}>
+      <Modal isOpen={isModalOpen} title={'Set target temperature for Zone'} onSubmit={()=>{update(state);openModal(false)}} onCancel={()=>{openModal(false)}}>
         <span className='plus' onClick={()=>{setState(state-0.1)}}>-</span>
         <span contentEditable='true' onChange={(e)=>setState(e.target.value)}>{state.toFixed(1)}</span>
         <span className='plus' onClick={()=>{setState(state+0.1)}}>+</span>
