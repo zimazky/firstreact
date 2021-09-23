b9810ce8ecc224f2f4e0744a873441a3bf425cee
andrew zimatskiy <zimazky@gmail.com>
2021-09-23 23:29:30 +0300

    Доработан скролл TimeTable

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -20,6 +20,7 @@ const colTime = {width:60}
 const colValue = {width:30}
 export function TimeTable({title = 'Table', headerName = 'value', data = [], time, height=200}) {
   const [row,setRow] = React.useState(data.findIndex(v=>v.time>=time))
+  const tableElement = React.useRef(null)
   const refTime = React.useRef(time)
   if(refTime.current!=time) { 
     refTime.current = time
@@ -33,22 +34,58 @@ export function TimeTable({title = 'Table', headerName = 'value', data = [], tim
   let totalRows = data.length
   let thumbHeight = height*(rows/totalRows)
   thumbHeight = thumbHeight<5?5:thumbHeight
-  let thumbShift = height*(row/totalRows)
+  let thumbShift = (height-thumbHeight)*(row/totalRows)
 
   function pgUp(e) {
     e.stopPropagation()
-    setRow(v=>v-rows)
-    //dispatch({type: 'changeRow', value: -rows})
+    setRow(v=>(v-rows)<0?0:v-rows)
   }
   function pgDown(e) {
-    setRow(v=>v+rows)
-    //dispatch({type: 'changeRow', value: rows})
+    setRow(v=>(v+rows)>totalRows?totalRows:v+rows)
   }
+  function onWheel(e) {
+    e.preventDefault()
+    let d = e.wheelDelta>0 ? -5 : 5
+    setRow(v=>v+d)
+  }
+  const refDrag = React.useRef({isDragging:false, y:0, row})
+  function onMouseDown(e) {
+    e.preventDefault()
+    refDrag.current = {isDragging:true, y:e.clientY, row}
+    console.log('mouseDown',e)
+  }
+  function onMouseMove(e) {
+    if (refDrag.current.isDragging) {
+      let deltaRow = totalRows*(e.clientY-refDrag.current.y)/height
+      let newRow = refDrag.current.row + deltaRow
+      if(newRow<0) newRow = 0
+      if(newRow>(totalRows-rows)) newRow = totalRows-rows
+      setRow(~~newRow)
+    }
+  }
+  function onMouseUp(e) {
+    //e.preventDefault()
+    refDrag.current = {isDragging:false, y:0}
+  }
+
+  React.useEffect(()=>{
+    console.log('TimeTable AddEventListener Mouse')
+    tableElement.current.addEventListener('wheel', onWheel)
+    document.addEventListener('mousemove',onMouseMove)
+    document.addEventListener('mouseup',onMouseUp)
+    return ()=>{
+      console.log('TimeRemoveEventListener Mouse')
+      tableElement.current.removeEventListener('wheel', onWheel)
+      document.removeEventListener('mousemove',onMouseMove)
+      document.removeEventListener('mouseup',onMouseUp)
+      }
+  },[])
+
   return (
     <>
     <div>{title}</div>
     <div className={classes.wrapper} style={{height}}>
-      <div className={classes.table}>
+      <div ref={tableElement} className={classes.table}>
       <div className={classes.row}>
         <span style={colId}>#</span><span style={colTime}>time</span><span style={colValue}>{headerName}</span>
       </div> { data.slice(row,row+rows).map((v,i)=>{
@@ -63,7 +100,10 @@ export function TimeTable({title = 'Table', headerName = 'value', data = [], tim
       </div>
       <div className={classes.scrollbarTrack} onClick={pgDown}>
         <div className={classes.scrollbarUp} style={{height:thumbShift}} onClick={pgUp}></div>
-        <div className={classes.scrollbarThumb} style={{height:thumbHeight}}></div>
+        <div className={classes.scrollbarThumb} style={{height:thumbHeight}} 
+          onMouseDown={onMouseDown} 
+          onClick={(e)=>{e.stopPropagation()}}
+        ></div>
       </div>
     </div>
     </>
