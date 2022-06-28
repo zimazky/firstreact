44b38786316a71f0c49131c8fa67b3e988841b8d
andrew zimatskiy <zimazky@gmail.com>
2022-06-28 20:51:17 +0300

    Добавлен парсинг параметров зон типа H и I в getinfo

--- src/arduinoapi/arduinoAPI.js
+++ src/arduinoapi/arduinoAPI.js
@@ -73,20 +73,42 @@ export default class ArduinoController {
           onControl:+m, powerOn:+p, targetTemperatureDelta: dt/10., sensorState:+s})
           return parseZonesInfo(i2, a, id)
         }
-        // 0:type, 1:id, 2:pressure
+        // 0:type, 1:id, 2:pressure, 3:mode, 4:hilimit, 5:lolimit, 6:drylimit, 7:pumpinittime, 8:pumprunlimit, 9:retryinterval
         // Пересчет значения датчика в бары
         // 0 бар соответствует значению 0,5 В или 102,3
         // 5 бар соответствует значению 4,5 В или 920,7
         // разница 4,5-0,5=4 В соответствует 818,4
+        // 0. Тип зоны контроллера
+        // 1. Идентификатор зоны
+        // 2. Давление
+        // 3. Режим
+        // 4. Верхняя граница
+        // 5. Нижняя граница
+        // 6. Граница сухого хода
+        // 7. Время инициализации насоса в секундах (для достижения давления выше предела сухого хода)
+        // 8. Предел времени непрерывной работы насоса в секундах (исключение длительной работы на сухом ходу)
+        // 9. Интервал времени для повторения запуска насоса в секундах (при сухом колодце)
         case 'H': {
-          const [id, p, ...i2] = data
-          a.push({type, id:+id, pressure: (p*10.-1023)*5./8184})
+          const [id, p, m, hi, lo, dry, pit, prl, ri, ...i2] = data
+          a.push({type, id:+id, pressure: (p*10.-1023)*5./8184, mode:+m
+            ,hilimit:+hi, lolimit:+lo, drylimit:+dry, pumpinittime:+pit
+            , pumprunlimit:+prl, retryinterval:+ri})
           return parseZonesInfo(i2, a, id)
         }
-        // 0:type, 1:id
+        // 0:type, 1:id, 2:p, 3:s, 4:d, 5:ds, 6:md
+        // 0. Тип зоны контроллера
+        // 1. Идентификатор зоны
+        // 2. Состояние программ и подачи мощности на клапан (poweron)
+        // 3. Параметры начала программ полива (start)
+        //    (каждый байт соответствует программе)
+        // 4. Параметры продолжительности программ полива (duration)
+        //    (каждый байт соответствует программе)
+        // 5. Параметры дней полива в программах (days)
+        //    (каждый байт соответствует программе)
+        // 6. Продолжительность ручного полива (mduration)
         case 'I': {
-          const [id, ...i2] = data
-          a.push({type, id:+id})
+          const [id, p, s, d, ds, md, ...i2] = data
+          a.push({type, id:+id, poweron:+p, start:+s, duration:+d, days:+ds, mduration:+md})
           return parseZonesInfo(i2, a, id)
         }
 
